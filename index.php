7d65f094e89e5fd7b1aa9df25b13308c81ddc0b6
marwahaha@berkeley.edu <marwahaha@berkeley.edu>
2015-02-19 05:59:00 +0000

    Pushed from Dropbox

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?>
+ <?php
+ echo '<p>Hello World</p>';
+// require 'twilio-php/Services/Twilio.php';
+?>
  </body>
 </html>
--- twilio.php
+++ twilio.php
@@ -1,7 +1,9 @@
 <?php
+    // require 'twilio-php/Services/Twilio.php';
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    
 ?>
 <Response>
-    <Message>Hello, Mobile Monkey</Message>
+    <Message>Hello,<?php echo $_REQUEST['From'] ?> Monkey</Message>
 </Response>
